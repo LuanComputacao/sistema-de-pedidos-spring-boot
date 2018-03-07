cc2ec62e028832232c097d05c255c8db8125d5ab
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2018-03-06 21:37:56 -0300

    sistema-de-pedidos-spring-boot-8 Crete Itens do Pedido page

--- src/main/webapp/static/js/pedidos.js
+++ src/main/webapp/static/js/pedidos.js
@@ -11,8 +11,8 @@ Pedido = {
     component: $("<div class='js-pedido'></div>").html('\
     \<div>\
     \   <b>ID</b>: <span class="js-pedido-id"></span><br> \
-    \   <b>Cliente: </b>: <span class="js-pedido-cliente"></span><br> \
-    \   <b>Data: </b>: <span class="js-pedido-data"></span><br> \
+    \   <b>Cliente</b>: [<span class="js-pedido-cliente-cpf"></span>] - <span class="js-pedido-cliente-nome"></span><br> \
+    \   <b>Data</b>: <span class="js-pedido-data"></span><br> \
     \</div> \
     '),
 
@@ -33,7 +33,7 @@ Pedido = {
 
     models: {
         id: null,
-        clienteID: null,
+        cliente: {id: null, cfp: null, nome: null, sobrenome: null},
         dataPedido: null
     },
 
@@ -49,12 +49,12 @@ Pedido = {
 
     init: function () {
         this.models.id = this.elements.form.id.val() || this.elements.form.id.data('value') || null;
-        this.models.clienteID = this.elements.form.clienteID.val() || this.elements.form.clienteID.data('value');
+        this.models.cliente.id = this.elements.form.clienteID.val() || this.elements.form.clienteID.data('value');
         this.models.dataPedido = this.elements.form.dataPedido.val() || this.elements.form.dataPedido.data('value');
 
 
         this.elements.form.id.val(this.models.id);
-        this.elements.form.clienteID.val(this.models.clienteID);
+        this.elements.form.clienteID.val(this.models.cliente.id);
         this.elements.form.dataPedido.val(this.models.dataPedido);
     },
 
@@ -67,9 +67,9 @@ Pedido = {
             });
 
             form.clienteID.on("change", function (e) {
-                Pedido.models.clienteID = form.clienteID.val();
+                Pedido.models.cliente.id = form.clienteID.val();
             });
-            form.dataPedido.on("change",function (e) {
+            form.dataPedido.on("change", function (e) {
                 Pedido.models.dataPedido = form.dataPedido.val();
             });
         }
@@ -84,7 +84,7 @@ Pedido = {
                 dataType: "json",
                 data: JSON.stringify({
                     id: Pedido.models.id,
-                    clienteID: Pedido.models.clienteID,
+                    clienteID: Pedido.models.cliente.id,
                     dataPedido: Pedido.models.dataPedido
                 }),
                 statusCode: {
@@ -108,15 +108,16 @@ Pedido = {
 
         fill: function (dataPedido) {
             Pedido.models.id = dataPedido.id;
-            Pedido.models.clienteID = dataPedido.clienteID;
+            Pedido.models.cliente = dataPedido.cliente;
             Pedido.models.dataPedido = dataPedido.dataPedido;
         },
 
         fillComponent: function () {
             var component = Pedido.component;
             component.find('.js-pedido-id').text(Pedido.models.id);
-            component.find('.js-pedido-cliente').text(Pedido.models.clienteID);
-            component.find('.js-pedido-data').text(Pedido.models.dataPedido);
+            component.find('.js-pedido-cliente-cpf').text(Pedido.models.cliente.cpf);
+            component.find('.js-pedido-cliente-nome').text(Pedido.models.cliente.nome + ' ' + Pedido.models.cliente.sobrenome);
+            component.find('.js-pedido-data').text(new Date(Pedido.models.dataPedido).toLocaleDateString());
         },
 
         updated: function () {
